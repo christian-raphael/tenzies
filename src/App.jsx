7970d9a03cb35a0f2a9a9b0a212b3eea8dea433e
Christian Raphael <christianraphael.1309@gmail.com>
2026-02-15 19:59:06 -0300

    generating dice with random values

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import Die from "./components/Die"
+import React from "react"
 
 export default function App() {
+  const [dice, setDice] = React.useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+  }
+
+  const diceElements = dice.map(num => <Die value={num}/>)
+
   return (
     <main>
         <div className="dice-container">
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
-          <Die value={3}/>
+          {diceElements}
         </div>
     </main>
   )
